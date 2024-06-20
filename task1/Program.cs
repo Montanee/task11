8825badeae04c81ff85cc82646087691fc3008ad
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:40:55 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        if (!double.TryParse(Console.ReadLine(), out double firstNumber))
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите число.");
+            return;
+        }
+
+        Console.WriteLine("Введите второе число:");
+        if (!double.TryParse(Console.ReadLine(), out double secondNumber))
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите число.");
+            return;
+        }
+
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine("Введенные числа равны.");
+        }
+        else if (firstNumber > secondNumber)
+        {
+            Console.WriteLine("Первое число больше второго.");
+        }
+        else
+        {
+            Console.WriteLine("Первое число меньше второго.");
+        }
+    }
+}
